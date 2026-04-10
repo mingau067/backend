7591fc1bc748a0665b8c467f47f0654479f7a11e
mingau067 <miguel.ayamfaz@gmail.com>
2026-04-09 21:53:55 -0300

    Update server listen address to '0.0.0.0'

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-require("dotenv").config();
 const express = require("express");
 
 const app = express();
@@ -37,7 +36,7 @@ app.get("/onlinecheck", (req, res) => {
     res.json({ online: true });
 });
 
-// 🚀 START
-app.listen(PORT, () => {
+// 🚀 START (IMPORTANTE)
+app.listen(PORT, "0.0.0.0", () => {
     console.log("Backend rodando na porta " + PORT);
 });
